4f32a2189d8ef3cf1f14e6284d933cefdfeb3629
BlueAnt1 <plaids.twosome.0d@icloud.com>
2024-10-31 15:09:23 -0400

    Tall boxShapes (for small sizes) are not allowed.

--- Sources/PuzzleCoding/Grid/Size.swift
+++ Sources/PuzzleCoding/Grid/Size.swift
@@ -6,34 +6,36 @@
 //
 
 /// The size of a grid.
-public enum Size: Int, Equatable, Hashable, CaseIterable, Sendable, CustomStringConvertible {
-    // raw values are used to encode the size in the encoding header
-    case shape2x3 = 5, shape2x4 = 6, shape3x2 = 7, shape4x2 = 8
-    case shape3x3 = 9, shape4x4 = 16, shape5x5 = 25
+public enum Size: RawRepresentable, Equatable, Hashable, CaseIterable, Sendable, CustomStringConvertible {
+    case grid6x6, grid8x8, grid9x9, grid16x16, grid25x25
 
     /// The number of rows in a box.
-    public var boxRowCount: Int { Self.dimensions[self]!.boxRowCount }
+    public var boxRowCount: Int { dimensions.boxRowCount }
     /// The number of columns in a box.
-    public var boxColumnCount: Int { Self.dimensions[self]!.boxColumnCount }
+    public var boxColumnCount: Int { dimensions.boxColumnCount }
     /// The number of cells in a house.
     public var houseCellCount: Int { boxRowCount * boxColumnCount }
     /// The number of cells in the grid.
     public var gridCellCount: Int { houseCellCount * houseCellCount }
     /// The range of clues, values & candidates that can be stored in the grid.
     public var valueRange: ClosedRange<Int> { 1...houseCellCount }
-    
-    private static let dimensions: [Size : (boxRowCount: Int, boxColumnCount: Int)] = [
-        .shape2x3: (2, 3),
-        .shape2x4: (2, 4),
-        .shape3x2: (3, 2),
-        .shape4x2: (4, 2),
-        .shape3x3: (3, 3),
-        .shape4x4: (4, 4),
-        .shape5x5: (5, 5)
-    ]
 
-    /// A 9×9 grid.
-    public static var grid9x9: Size { .shape3x3 }
-    
+    private var dimensions: (boxRowCount: Int, boxColumnCount: Int) {
+        switch self {
+        case .grid6x6: return (2, 3)
+        case .grid8x8: return (2, 4)
+        case .grid9x9: return (3, 3)
+        case .grid16x16: return (4, 4)
+        case .grid25x25: return (5, 5)
+        }
+    }
+
+    public var rawValue: Int { houseCellCount }
+
+    public init?(rawValue: Int) {
+        guard let size = Self.allCases.first(where: { $0.rawValue == rawValue }) else { return nil }
+        self = size
+    }
+
     public var description: String { "\(houseCellCount)×\(houseCellCount)" }
 }
