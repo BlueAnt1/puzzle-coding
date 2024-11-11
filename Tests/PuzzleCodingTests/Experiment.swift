13a4f598cad0c88a8bab057562c877eff85ce891
BlueAnt1 <plaids.twosome.0d@icloud.com>
2024-11-11 10:33:05 -0500

    Refactor Experiment

--- Tests/PuzzleCodingTests/Experiment.swift
+++ Tests/PuzzleCodingTests/Experiment.swift
@@ -38,9 +38,9 @@ struct ExperimentNoOptimization {
             switch self {
             case .kendoku: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
             case .kenken: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .killerJigsaw: [\.boxShapes, \.cageClues, \.cageShapes, \.progress]
+            case .killerJigsaw: [\.boxShapes, \.boxColors, \.cageClues, \.cageShapes, \.progress]
             case .killerSudoku: [\.cageClues, \.cageShapes, \.progress]
-            case .jigsawSudoku: [\.clues, \.boxShapes, \.progress]
+            case .jigsawSudoku: [\.clues, \.boxShapes, \.boxColors, \.progress]
             case .sudoku: [\.clues, \.progress]
             }
         }
@@ -50,6 +50,7 @@ struct ExperimentNoOptimization {
         let size: Size
         var clues: [Int]? = nil
         var boxShapes: [Int]? = nil
+        var boxColors: [Int]? = nil
         var cageClues: [Int]? = nil
         var cageShapes: [Int]? = nil
         var clueOperator: [Int]? = nil
@@ -59,6 +60,7 @@ struct ExperimentNoOptimization {
             [
                 \.clues: 0...size.valueRange.upperBound,
                  \.boxShapes: size.valueRange,
+                 \.boxColors: 1...5,
                  \.cageClues: 0...size.valueRange.reduce(0, +),
                  \.cageShapes: 1...5,
                  \.clueOperator: 1...4,
@@ -68,25 +70,7 @@ struct ExperimentNoOptimization {
     }
 }
 
-struct ExperimentClueWithProgressBase36 {
-    @Test
-    func analyze() {
-        let sizes = Size.allCases   // [Size.grid9x9]
-        let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
-        print("Size", "Puzzle", "Range", "Bits", "Field Width", "Encoding", separator: "\t")
-        for size in sizes {
-            let puzzle = Puzzle(size: size)
-            for type in puzzles {
-                let ranges = type.requirements.map { puzzle.ranges[$0]! }
-                let transform = ShiftTransform(ranges: ranges)
-                let bitCount = String(transform.range.upperBound, radix: 2).count
-                let fieldWidth = String(transform.range.upperBound, radix: 36).count
-                let encodingCount = fieldWidth * size.gridCellCount
-                print(size, type, transform.range, bitCount, fieldWidth, encodingCount, separator: "\t")
-            }
-        }
-    }
-
+private enum Experiment {
     enum PuzzleType: CaseIterable {
         typealias Requirement = KeyPath<Puzzle, [Int]?>
         typealias Requirements = [Requirement]
@@ -97,9 +81,9 @@ struct ExperimentClueWithProgressBase36 {
             switch self {
             case .kendoku: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
             case .kenken: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .killerJigsaw: [\.boxShapes, \.cageClues, \.cageShapes, \.progress]
+            case .killerJigsaw: [\.boxShapes, \.boxColors, \.cageClues, \.cageShapes, \.progress]
             case .killerSudoku: [\.cageClues, \.cageShapes, \.progress]
-            case .jigsawSudoku: [\.boxShapes, \.progress]
+            case .jigsawSudoku: [\.boxShapes, \.boxColors, \.progress]
             case .sudoku: [\.progress]
             }
         }
@@ -109,6 +93,7 @@ struct ExperimentClueWithProgressBase36 {
         let size: Size
         var clues: [Int]? = nil
         var boxShapes: [Int]? = nil
+        var boxColors: [Int]? = nil
         var cageClues: [Int]? = nil
         var cageShapes: [Int]? = nil
         var clueOperator: [Int]? = nil
@@ -118,6 +103,7 @@ struct ExperimentClueWithProgressBase36 {
             [
                 \.clues: 0...size.valueRange.upperBound,
                  \.boxShapes: size.valueRange,
+                 \.boxColors: 1...5,
                  \.cageClues: 0...size.valueRange.reduce(0, +),
                  \.cageShapes: 1...5,
                  \.clueOperator: 1...4,
@@ -127,64 +113,49 @@ struct ExperimentClueWithProgressBase36 {
     }
 }
 
-struct ExperimentPackMultipleCellsIntoOneInt {
-    @Test
-    func analyze() {
-        let sizes = Size.allCases   // [Size.grid9x9]
-        let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
-        print("Size", "Puzzle", "Range", "Bits", "Pack", "Field Width", "Encoding", separator: "\t")
-        for size in sizes {
-            let puzzle = Puzzle(size: size)
-            for type in puzzles {
-                let ranges = type.requirements.map { puzzle.ranges[$0]! }
-                let transform = ShiftTransform(ranges: ranges)
-                let bitCount = String(transform.range.upperBound, radix: 2).count
-                let (packCount, extra) = 64.quotientAndRemainder(dividingBy: bitCount)
-                let upperBound: UInt64 = (1...packCount).reduce(0) { result, _ in (result << UInt64(bitCount)) + UInt64(transform.range.upperBound) }
-
-                let fieldWidth = String(upperBound, radix: 36).count
-                let encodingCount = fieldWidth * (size.gridCellCount / packCount + extra.signum())
-                print(size, type, transform.range, bitCount, packCount, fieldWidth, encodingCount, separator: "\t")
+extension Experiment {
+    struct ClueWithProgressBase36 {
+        @Test
+        func analyze() {
+            let sizes = Size.allCases   // [Size.grid9x9]
+            let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
+            print("Size", "Puzzle", "Range", "Bits", "Field Width", "Encoding", separator: "\t")
+            for size in sizes {
+                let puzzle = Puzzle(size: size)
+                for type in puzzles {
+                    let ranges = type.requirements.map { puzzle.ranges[$0]! }
+                    let transform = ShiftTransform(ranges: ranges)
+                    let bitCount = String(transform.range.upperBound, radix: 2).count
+                    let fieldWidth = String(transform.range.upperBound, radix: 36).count
+                    let encodingCount = fieldWidth * size.gridCellCount
+                    print(size, type, transform.range, bitCount, fieldWidth, encodingCount, separator: "\t")
+                }
             }
         }
     }
+}
 
-    enum PuzzleType: CaseIterable {
-        typealias Requirement = KeyPath<Puzzle, [Int]?>
-        typealias Requirements = [Requirement]
+extension Experiment {
+    struct PackMultipleCellsIntoOneInt {
+        @Test
+        func analyze() {
+            let sizes = Size.allCases   // [Size.grid9x9]
+            let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
+            print("Size", "Puzzle", "Range", "Bits", "Pack", "Field Width", "Encoding", separator: "\t")
+            for size in sizes {
+                let puzzle = Puzzle(size: size)
+                for type in puzzles {
+                    let ranges = type.requirements.map { puzzle.ranges[$0]! }
+                    let transform = ShiftTransform(ranges: ranges)
+                    let bitCount = String(transform.range.upperBound, radix: 2).count
+                    let (packCount, extra) = 64.quotientAndRemainder(dividingBy: bitCount)
+                    let upperBound: UInt64 = (1...packCount).reduce(0) { result, _ in (result << UInt64(bitCount)) + UInt64(transform.range.upperBound) }
 
-        case kendoku, kenken, killerJigsaw, killerSudoku, jigsawSudoku, sudoku
-
-        var requirements: Requirements {
-            switch self {
-            case .kendoku: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .kenken: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .killerJigsaw: [\.boxShapes, \.cageClues, \.cageShapes, \.progress]
-            case .killerSudoku: [\.cageClues, \.cageShapes, \.progress]
-            case .jigsawSudoku: [\.boxShapes, \.progress]
-            case .sudoku: [\.progress]
+                    let fieldWidth = String(upperBound, radix: 36).count
+                    let encodingCount = fieldWidth * (size.gridCellCount / packCount + extra.signum())
+                    print(size, type, transform.range, bitCount, packCount, fieldWidth, encodingCount, separator: "\t")
+                }
             }
-        }
-    }
-
-    struct Puzzle {
-        let size: Size
-        var clues: [Int]? = nil
-        var boxShapes: [Int]? = nil
-        var cageClues: [Int]? = nil
-        var cageShapes: [Int]? = nil
-        var clueOperator: [Int]? = nil
-        var progress: [Int]? = nil
-
-        var ranges: [KeyPath<Puzzle, [Int]?> : ClosedRange<Int>] {
-            [
-                \.clues: 0...size.valueRange.upperBound,
-                 \.boxShapes: size.valueRange,
-                 \.cageClues: 0...size.valueRange.reduce(0, +),
-                 \.cageShapes: 1...5,
-                 \.clueOperator: 1...4,
-                 \.progress: CellContentTransform(size: size).range
-            ]
         }
     }
 }
@@ -201,145 +172,70 @@ private extension UInt64 {
     }
 }
 
-struct ExperimentCompressB64 {
-    @Test
-    func analyze() {
-        let sizes = Size.allCases   // [Size.grid9x9]
-        let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
-        print("Size", "Puzzle", "Range", "Bits", "Encoding", separator: "\t")
-        for size in sizes {
-            let puzzle = Puzzle(size: size)
-            for type in puzzles {
-                let ranges = type.requirements.map { puzzle.ranges[$0]! }
-                let transform = ShiftTransform(ranges: ranges)
-                let bitCount = String(transform.range.upperBound, radix: 2).count
+extension Experiment {
+    struct CompressB64 {
+        @Test
+        func analyze() {
+            let sizes = Size.allCases   // [Size.grid9x9]
+            let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
+            print("Size", "Puzzle", "Range", "Bits", "Encoding", separator: "\t")
+            for size in sizes {
+                let puzzle = Puzzle(size: size)
+                for type in puzzles {
+                    let ranges = type.requirements.map { puzzle.ranges[$0]! }
+                    let transform = ShiftTransform(ranges: ranges)
+                    let bitCount = String(transform.range.upperBound, radix: 2).count
 
-                var encodingCountAverage: Int {
-                    var counts = [Int]()
-                    for _ in 0 ..< 100 {
-                        let bytes = (0..<size.gridCellCount).flatMap { _ in UInt64(transform.range.randomElement()!).littleEndianBytes }
-                        let data = Data(bytes)
-                        let compressed = try! (data as NSData).compressed(using: .zlib) as Data
-                        let b64 = compressed.base64EncodedString()
-                        counts.append(b64.count)
+                    var encodingCountAverage: Int {
+                        var counts = [Int]()
+                        for _ in 0 ..< 100 {
+                            let bytes = (0..<size.gridCellCount).flatMap { _ in UInt64(transform.range.randomElement()!).littleEndianBytes }
+                            let data = Data(bytes)
+                            let compressed = try! (data as NSData).compressed(using: .zlib) as Data
+                            let b64 = compressed.base64EncodedString()
+                            counts.append(b64.count)
+                        }
+                        return counts.reduce(0, +) / counts.count
                     }
-                    return counts.reduce(0, +) / counts.count
+
+                    print(size, type, transform.range, bitCount, encodingCountAverage, separator: "\t")
                 }
-
-                print(size, type, transform.range, bitCount, encodingCountAverage, separator: "\t")
             }
-        }
-    }
-
-    enum PuzzleType: CaseIterable {
-        typealias Requirement = KeyPath<Puzzle, [Int]?>
-        typealias Requirements = [Requirement]
-
-        case kendoku, kenken, killerJigsaw, killerSudoku, jigsawSudoku, sudoku
-
-        var requirements: Requirements {
-            switch self {
-            case .kendoku: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .kenken: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .killerJigsaw: [\.boxShapes, \.cageClues, \.cageShapes, \.progress]
-            case .killerSudoku: [\.cageClues, \.cageShapes, \.progress]
-            case .jigsawSudoku: [\.boxShapes, \.progress]
-            case .sudoku: [\.progress]
-            }
-        }
-    }
-
-    struct Puzzle {
-        let size: Size
-        var clues: [Int]? = nil
-        var boxShapes: [Int]? = nil
-        var cageClues: [Int]? = nil
-        var cageShapes: [Int]? = nil
-        var clueOperator: [Int]? = nil
-        var progress: [Int]? = nil
-
-        var ranges: [KeyPath<Puzzle, [Int]?> : ClosedRange<Int>] {
-            [
-                \.clues: 0...size.valueRange.upperBound,
-                 \.boxShapes: size.valueRange,
-                 \.cageClues: 0...size.valueRange.reduce(0, +),
-                 \.cageShapes: 1...5,
-                 \.clueOperator: 1...4,
-                 \.progress: CellContentTransform(size: size).range
-            ]
         }
     }
 }
 
-struct ExperimentCompressB36 {
-    @Test
-    func analyze() {
-        let sizes = Size.allCases   // [Size.grid9x9]
-        let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
-        print("Size", "Puzzle", "Range", "Bits", "Encoding", separator: "\t")
-        for size in sizes {
-            let puzzle = Puzzle(size: size)
-            for type in puzzles {
-                let ranges = type.requirements.map { puzzle.ranges[$0]! }
-                let transform = ShiftTransform(ranges: ranges)
-                let bitCount = String(transform.range.upperBound, radix: 2).count
+extension Experiment {
+    struct CompressB36 {
+        @Test
+        func analyze() {
+            let sizes = Size.allCases   // [Size.grid9x9]
+            let puzzles = PuzzleType.allCases   // [PuzzleType.sudoku]
+            print("Size", "Puzzle", "Range", "Bits", "Encoding", separator: "\t")
+            for size in sizes {
+                let puzzle = Puzzle(size: size)
+                for type in puzzles {
+                    let ranges = type.requirements.map { puzzle.ranges[$0]! }
+                    let transform = ShiftTransform(ranges: ranges)
+                    let bitCount = String(transform.range.upperBound, radix: 2).count
 
-                var encodingCountAverage: Int {
-                    var counts = [Int]()
-                    for _ in 0 ..< 100 {
-                        let bytes = (0..<size.gridCellCount).flatMap { _ in UInt64(transform.range.randomElement()!).littleEndianBytes }
-                        let data = Data(bytes)
-                        let compressed = try! (data as NSData).compressed(using: .zlib) as Data
-                        // UInt64.max, base 36, field width = 13 characters
-                        let (ints, extra) = compressed.count.quotientAndRemainder(dividingBy: 8)  // 8 bytes per Int
-                        let encodingCount = (ints + extra.signum()) * 13
-                        counts.append(encodingCount)
+                    var encodingCountAverage: Int {
+                        var counts = [Int]()
+                        for _ in 0 ..< 100 {
+                            let bytes = (0..<size.gridCellCount).flatMap { _ in UInt64(transform.range.randomElement()!).littleEndianBytes }
+                            let data = Data(bytes)
+                            let compressed = try! (data as NSData).compressed(using: .zlib) as Data
+                            // UInt64.max, base 36, field width = 13 characters
+                            let (ints, extra) = compressed.count.quotientAndRemainder(dividingBy: 8)  // 8 bytes per Int
+                            let encodingCount = (ints + extra.signum()) * 13
+                            counts.append(encodingCount)
+                        }
+                        return counts.reduce(0, +) / counts.count
                     }
-                    return counts.reduce(0, +) / counts.count
+
+                    print(size, type, transform.range, bitCount, encodingCountAverage, separator: "\t")
                 }
-
-                print(size, type, transform.range, bitCount, encodingCountAverage, separator: "\t")
             }
-        }
-    }
-
-    enum PuzzleType: CaseIterable {
-        typealias Requirement = KeyPath<Puzzle, [Int]?>
-        typealias Requirements = [Requirement]
-
-        case kendoku, kenken, killerJigsaw, killerSudoku, jigsawSudoku, sudoku
-
-        var requirements: Requirements {
-            switch self {
-            case .kendoku: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .kenken: [\.cageClues, \.cageShapes, \.clueOperator, \.progress]
-            case .killerJigsaw: [\.boxShapes, \.cageClues, \.cageShapes, \.progress]
-            case .killerSudoku: [\.cageClues, \.cageShapes, \.progress]
-            case .jigsawSudoku: [\.boxShapes, \.progress]
-            case .sudoku: [\.progress]
-            }
-        }
-    }
-
-    struct Puzzle {
-        let size: Size
-        var clues: [Int]? = nil
-        var boxShapes: [Int]? = nil
-        var cageClues: [Int]? = nil
-        var cageShapes: [Int]? = nil
-        var clueOperator: [Int]? = nil
-        var progress: [Int]? = nil
-
-        var ranges: [KeyPath<Puzzle, [Int]?> : ClosedRange<Int>] {
-            [
-                \.clues: 0...size.valueRange.upperBound,
-                 \.boxShapes: size.valueRange,
-                 \.cageClues: 0...size.valueRange.reduce(0, +),
-                 \.cageShapes: 1...5,
-                 \.clueOperator: 1...4,
-                 \.progress: CellContentTransform(size: size).range
-            ]
         }
     }
 }
-
